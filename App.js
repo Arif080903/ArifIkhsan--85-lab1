8358cdc03449cf7643e7a7cf9b2faafc1011938f
Izumi <105841108521@student.unismuh.ac.id>
2024-06-12 15:50:22 +0800

    Add Component Button

--- App.js
+++ App.js
@@ -1,27 +1,8 @@
 import { StyleSheet, Text, View, Image} from 'react-native'
 import React from 'react'
+import ButtonCostum from './Component/Button/Button'
 
 const App = () => {
-
-  const ButtonCostum = ({color, text}) => {
-  return(
-    <View style={{
-      backgroundColor : color,
-      width : 120,
-      height : 80,
-      borderRadius : 20,
-      justifyContent : 'center'
-
-    }}>
-      <Text style={{
-        textAlign : 'center',
-        fontSize : 30,
-        color : 'white',
-        fontWeight : 'bold'
-      }}>{text}
-      </Text>
-    </View>
-  )}
   return(
     <View style={{flex : 1}}>
       <View style={{
