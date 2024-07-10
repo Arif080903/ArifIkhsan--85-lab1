14ed8c38ba96167dead1261f500ae95df61c379a
Izumi <105841108521@student.unismuh.ac.id>
2024-07-10 14:58:13 +0800

    Navigation and Taskbar 2

--- App.js
+++ App.js
@@ -1,10 +1,62 @@
 import * as React from 'react';
-import { View, Text, Button} from 'react-native';
+import { View, Text, Button, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeAktif from './assets/Icon/HomeAktif.jpeg'
+import HomeInaktif from './assets/Icon/HomeInaktif.jpeg'
+import Shop from './assets/Icon/Shop.jpeg'
+import ShopInaktif from './assets/Icon/ShopInaktif.jpeg'
 import LoginPage from './LoginPage'
 import SignUp from './SignUp'
 import Forgot from './Forgot'
+
+const Tab = createBottomTabNavigator();
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? HomeAktif : HomeInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      
+
+      <Tab.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? Shop : ShopInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+
+<Tab.Screen
+        name="Login"
+        component={LoginPage}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? Shop : ShopInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 function HomeScreen({navigation}) {
   return (
@@ -21,9 +73,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
+        <Stack.Screen name="SignUp" component={MyTabs} options={{headerShown: false}} />
         <Stack.Screen name="LoginPage" component={LoginPage} options={{headerShown: false}} />
-        <Stack.Screen name="Forgot" component={Forgot} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
